bef569ffb703b0ec23ac7c453875209408a48aab
NAKAGAKI Kenji <nakaken0629@gmail.com>
2017-02-26 00:37:21 +0900

    S3からjQueryでAPI Gatewayにアクセスする処理を作成

--- handler.js
+++ handler.js
@@ -3,6 +3,10 @@
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+      "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS 
+    },
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
       input: event,
